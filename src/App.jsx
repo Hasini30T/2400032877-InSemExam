d3a181cd28cce8039ba2128e5db3b720ba7658e1
Hasini <hasini3064@gmail.com>
2025-10-15 17:37:38 +0530

    created weather app using reactjs

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState('');
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+  const handleSearch = async () => {
+    if (!city) return;
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      setWeather(response.data);
+      setError('');
+    } catch (err) {
+      setError('City not found or API error.');
+      setWeather(null);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city"
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {error && <p className="error">{error}</p>}
+
+      {weather && (
+        <div className="weather-info">
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          <p>Weather Condition: {weather.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
